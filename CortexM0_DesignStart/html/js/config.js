e3d7a4c51a746a7473be006e8172c004e67a600f
OyinkuroBenafa <81170416+OyinkuroBenafa@users.noreply.github.com>
2021-11-11 14:06:55 +0000

    Initial commit

---
+++ CortexM0_DesignStart/html/js/config.js
@@ -0,0 +1,234 @@
+var index = 
+{
+  "titles": {
+    "title": "Arm Education Kit",
+    "libname": "",
+    "libtype": "Introduction to System-on-Chip Design"
+  },
+  "headers": [
+    {
+      "name": "Lecture Presentation",
+      "column": "modules"
+    },
+    {
+      "name": "Lab Materials",
+      "column": "exercises"
+    }
+  ],
+  "modules": [
+    [
+    {
+        "column": "modules",
+        "name": "Module 1: Introduction to Arm-based System-on-Chip Design",
+        "link": "contents/Module_1/Lecture_1_intro.pptx"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 2: The Arm Cortex-M0 Processor Architecture: Part 1",
+        "link": "contents/Module_2/Lecture_2_Cortex-M0_part1.pptx"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 3: The Arm Cortex-M0 Processor Architecture: Part 2",
+        "link": "contents/Module_3/Lecture_3_Cortex-M0_part2.pptx"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 4: AMBA 3 AHB-Lite Bus Architecture",
+        "link": "contents/Module_4/Lecture_4_AHB-Lite.pptx"
+      },
+      {
+        "column": "exercises",
+        "name": "Getting Started Guide",
+        "link": "contents/Module_4/Lab/Intro_SOC_Getting_started.docx"
+      },
+      {
+        "column": "exercises",
+        "name": "Lab 4",
+        "link": "contents/Module_4/Lab/IntroSoC_lab04.docx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab 4 Files",
+        "link": "contents/Module_4/Lab/lab_files.zip"
+      }
+    ],
+    [
+    {
+        "column": "modules",
+        "name": "Module 5: AHB VGA Peripheral",
+        "link": "contents/Module_5/Lecture_5_VGA.pptx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab 5",
+        "link": "contents/Module_5/Lab/IntroSoC_lab05.docx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab 5 Files",
+        "link": "contents/Module_5/Lab/lab_files.zip"
+      },
+    {
+        "column": "exercises",
+        "name": "Solution files for lab 5",
+        "link": "contents/Module_5/Lab/Lab_Code_Solution.zip"
+      }
+    ],
+    [
+	  {
+        "column": "modules",
+        "name": "Module 6: AHB UART Peripheral",
+        "link": "contents/Module_6/Lecture_6_UART.pptx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab 6",
+        "link": "contents/Module_6/Lab/IntroSoC_lab06.docx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab 6 Files",
+        "link": "contents/Module_6/Lab/lab_files.zip"
+      },
+    {
+        "column": "exercises",
+        "name": "Solution files for lab 6",
+        "link": "contents/Module_6/Lab/Lab_Code_Solution.zip"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 7: Timer, GPIO  and 7-Segment Peripherals",
+        "link": "contents/Module_7/Lecture_7_Timer_GPIO_7seg.pptx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab 7",
+        "link": "contents/Module_7/Lab/IntroSoC_lab07.docx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab 7 Files",
+        "link": "contents/Module_7/Lab/lab_files.zip"
+      },
+    {
+        "column": "exercises",
+        "name": "Solution files for lab 7",
+        "link": "contents/Module_7/Lab/Lab_Code_Solution.zip"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 8: Interrupt Mechanisms",
+        "link": "contents/Module_8/Lecture_8_Interrupts.pptx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab 8",
+        "link": "contents/Module_8/Lab/IntroSoC_lab08.docx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab 8 Files",
+        "link": "contents/Module_8/Lab/lab_files.zip"
+      },
+    {
+        "column": "exercises",
+        "name": "Solution files for lab 8",
+        "link": "contents/Module_8/Lab/Lab_Code_Solution.zip"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 9: Programming an SoC using C Language",
+        "link": "contents/Module_9/Lecture_9_using_C.pptx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab 9",
+        "link": "contents/Module_9/Lab/IntroSoC_lab09.docx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab 9 Files",
+        "link": "contents/Module_9/Lab/lab_files.zip"
+      },
+    {
+        "column": "exercises",
+        "name": "Solution files for lab 9",
+        "link": "contents/Module_9/Lab/Lab_Code_Solution.zip"
+      }
+    ],
+    [
+	  {
+        "column": "modules",
+        "name": "Module 10: Arm CMSIS and Software Drivers",
+        "link": "contents/Module_10/Lecture_10_CMSIS.pptx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab 10",
+        "link": "contents/Module_10/Lab/IntroSoC_lab10.docx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab 10 Files",
+        "link": "contents/Module_10/Lab/lab_files.zip"
+      },
+    {
+        "column": "exercises",
+        "name": "Solution files for lab 10",
+        "link": "contents/Module_10/Lab/Lab_Code_Solution.zip"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 11: Application Programming Interface and Final Application",
+        "link": "contents/Module_11/Lecture_11_API_Snake.pptx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab 11",
+        "link": "contents/Module_11/Lab/IntroSoC_lab11.docx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab 11 Files",
+        "link": "contents/Module_11/Lab/lab_files.zip"
+      },
+    {
+        "column": "exercises",
+        "name": "Solution files for lab 11",
+        "link": "contents/Module_11/Lab/Lab_Code_Solution.zip"
+      }
+    ],
+    [
+     {
+        "column": "exercises",
+        "name": "Lab 12",
+        "link": "contents/Module_12/Lab/IntroSoC_lab12.docx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab 12 Files",
+        "link": "contents/Module_12/Lab/lab_files.zip"
+      },
+    {
+        "column": "exercises",
+        "name": "Solution files for lab 12",
+        "link": "contents/Module_12/Lab/Lab_Code_Solution.zip"
+      }
+    ]
+  ]
+}
---
+++ README.md
@@ -0,0 +1,49 @@
+
+# Introduction-to-System-on-Chip-Design-Education-Kit
+
+Welcome to our Introduction to System on Chip Design Education Kit!
+
+### [Download the EdKit here](https://github.com/arm-university/Introduction-to-SoC-Design-Education-Kit/archive/refs/heads/main.zip)
+
+Our flagship offering to universities worldwide is the Arm University Program Education Kit series.
+
+These self-contained educational materials offered exclusively and at no cost to academics and teaching staff worldwide. They’re designed to support your day-to-day teaching on core electronic engineering and computer science subjects. You have the freedom to choose which modules to teach – you can use all the modules in the Education Kit or only those that are most appropriate to your teaching outcomes.
+
+Our Introduction to System on Chip Design Education Kit covers the fundamentals of System-on-Chip design, including how the theories and concepts can be applied in the design and programming of a simple SoC implemented on an FPGA. A full description of the education kit can be found [here](https://www.arm.com/resources/education/education-kits/introduction-to-soc).
+
+
+ ## Kit specification:
+
+* A full set of lecture slides, ready for use in a typical 10-12-week undergraduate course (full syllabus below).
+* Software licenses from Arm
+* Lab manual with solutions for faculty. Labs are based on low-cost yet powerful Arm-based hardware platforms donated by partners (subject to availability). 
+* **Prerequisites:** Basics of hardware description language (Verilog or VHDL), basic C and assembly programming.
+
+## Course Aim
+To produce students who can design and program Arm-based embedded systems and implement them using commercial API.
+
+## Syllabus
+1. Introduction to Arm-based System-on-Chip Design
+1. The Arm Cortex-M0 Processor Architecture: Part 1
+1. The Arm Cortex-M0 Processor Architecture: Part 2
+1. AMBA 3 AHB-Lite Bus Architecture
+1. AHB VGA Peripheral
+1. AHB UART Peripheral 
+1. Timer, GPIO and 7-Segment Peripherals
+1. Interrupt Mechanisms 
+1. Programming an SoC using C Language
+1. Arm CMSIS and Software Drivers
+1. Application Programming Interface and Final Application 
+
+
+## License
+You are free to fork or clone this material. See [LICENSE.md](https://github.com/arm-university/Introduction-to-SoC-Design-Education-Kit/blob/main/License/LICENSE.md) for the complete license.
+
+## Inclusive Language Commitment
+Arm is committed to making the language we use inclusive, meaningful, and respectful. Our goal is to remove and replace non-inclusive language from our vocabulary to reflect our values and represent our global ecosystem.
+ 
+Arm is working actively with our partners, standards bodies, and the wider ecosystem to adopt a consistent approach to the use of inclusive language and to eradicate and replace offensive terms. We recognise that this will take time. This course may contain references to non-inclusive language; it will be updated with newer terms as those terms are agreed and ratified with the wider community. We recognise that some of you will be accustomed to using the previous terms and may not immediately recognise their replacements. Please refer to the following examples:
+
+•	When introducing the AMBA 3 AHB-Lite Protocols, we will use the term ‘Manager’ instead of ‘Master’ and ‘Subordinate’ instead of ‘Slave’. 
+ 
+Contact us at education@arm.com with questions or comments about this course. You can also report non-inclusive and offensive terminology usage in Arm content at terms@arm.com.
