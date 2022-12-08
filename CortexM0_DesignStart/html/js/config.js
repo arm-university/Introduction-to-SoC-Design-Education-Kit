29f96c60c23c61e1e0d3df7c8cabe05ae3f81384
Richard Cook <r.a.cook@soton.ac.uk>
2022-12-08 17:18:53 +0000

    Fix paths in config.js

--- CortexM0_DesignStart/html/js/config.js
+++ CortexM0_DesignStart/html/js/config.js
@@ -20,214 +20,214 @@ var index =
     {
         "column": "modules",
         "name": "Module 1: Introduction to Arm-based System-on-Chip Design",
-        "link": "contents/Module_1/Lecture_1_intro.pptx"
+        "link": "contents/Module 01_Introduction to Arm-based System-on-Chip Design/Lecture_1_intro.pptx"
       }
     ],
     [
       {
         "column": "modules",
         "name": "Module 2: The Arm Cortex-M0 Processor Architecture: Part 1",
-        "link": "contents/Module_2/Lecture_2_Cortex-M0_part1.pptx"
+        "link": "contents/Module 02_The Arm Cortex-M0 Processor Architecture: Part 1/Lecture_2_Cortex-M0_part1.pptx"
       }
     ],
     [
       {
         "column": "modules",
         "name": "Module 3: The Arm Cortex-M0 Processor Architecture: Part 2",
-        "link": "contents/Module_3/Lecture_3_Cortex-M0_part2.pptx"
+        "link": "contents/Module 03_The Arm Cortex-M0 Processor Architecture: Part 2/Lecture_3_Cortex-M0_part2.pptx"
       }
     ],
     [
       {
         "column": "modules",
         "name": "Module 4: AMBA 3 AHB-Lite Bus Architecture",
-        "link": "contents/Module_4/Lecture_4_AHB-Lite.pptx"
+        "link": "contents/Module 04_AMBA 3 AHB-Lite Bus Architecture/Lecture_4_AHB-Lite.pptx"
       },
       {
         "column": "exercises",
         "name": "Getting Started Guide",
-        "link": "contents/Module_4/Lab/Intro_SOC_Getting_started.docx"
+        "link": "contents/Module 04_AMBA 3 AHB-Lite Bus Architecture/Lab/Intro_SOC_Getting_started.docx"
       },
       {
         "column": "exercises",
         "name": "Lab 4",
-        "link": "contents/Module_4/Lab/IntroSoC_lab04.docx"
+        "link": "contents/Module 04_AMBA 3 AHB-Lite Bus Architecture/Lab/IntroSoC_lab04.docx"
       },
     {
         "column": "exercises",
         "name": "Lab 4 Files",
-        "link": "contents/Module_4/Lab/lab_files.zip"
+        "link": "contents/Module 04_AMBA 3 AHB-Lite Bus Architecture/Lab/lab_files.zip"
       }
     ],
     [
     {
         "column": "modules",
         "name": "Module 5: AHB VGA Peripheral",
-        "link": "contents/Module_5/Lecture_5_VGA.pptx"
+        "link": "contents/Module 05_AHB VGA Peripheral/Lecture_5_VGA.pptx"
       },
     {
         "column": "exercises",
         "name": "Lab 5",
-        "link": "contents/Module_5/Lab/IntroSoC_lab05.docx"
+        "link": "contents/Module 05_AHB VGA Peripheral/Lab/IntroSoC_lab05.docx"
       },
     {
         "column": "exercises",
         "name": "Lab 5 Files",
-        "link": "contents/Module_5/Lab/lab_files.zip"
+        "link": "contents/Module 05_AHB VGA Peripheral/Lab/lab_files.zip"
       },
     {
         "column": "exercises",
         "name": "Solution files for lab 5",
-        "link": "contents/Module_5/Lab/Lab_Code_Solution.zip"
+        "link": "contents/Module 05_AHB VGA Peripheral/Lab/Lab_Code_Solution.zip"
       }
     ],
     [
 	  {
         "column": "modules",
         "name": "Module 6: AHB UART Peripheral",
-        "link": "contents/Module_6/Lecture_6_UART.pptx"
+        "link": "contents/Module 06_AHB UART Peripheral/Lecture_6_UART.pptx"
       },
     {
         "column": "exercises",
         "name": "Lab 6",
-        "link": "contents/Module_6/Lab/IntroSoC_lab06.docx"
+        "link": "contents/Module 06_AHB UART Peripheral/Lab/IntroSoC_lab06.docx"
       },
     {
         "column": "exercises",
         "name": "Lab 6 Files",
-        "link": "contents/Module_6/Lab/lab_files.zip"
+        "link": "contents/Module 06_AHB UART Peripheral/Lab/lab_files.zip"
       },
     {
         "column": "exercises",
         "name": "Solution files for lab 6",
-        "link": "contents/Module_6/Lab/Lab_Code_Solution.zip"
+        "link": "contents/Module 06_AHB UART Peripheral/Lab/Lab_Code_Solution.zip"
       }
     ],
     [
       {
         "column": "modules",
         "name": "Module 7: Timer, GPIO  and 7-Segment Peripherals",
-        "link": "contents/Module_7/Lecture_7_Timer_GPIO_7seg.pptx"
+        "link": "contents/Module 07_Timer, GPIO and 7-Segment Peripherals/Lecture_7_Timer_GPIO_7seg.pptx"
       },
     {
         "column": "exercises",
         "name": "Lab 7",
-        "link": "contents/Module_7/Lab/IntroSoC_lab07.docx"
+        "link": "contents/Module 07_Timer, GPIO and 7-Segment Peripherals/Lab/IntroSoC_lab07.docx"
       },
     {
         "column": "exercises",
         "name": "Lab 7 Files",
-        "link": "contents/Module_7/Lab/lab_files.zip"
+        "link": "contents/Module 07_Timer, GPIO and 7-Segment Peripherals/Lab/lab_files.zip"
       },
     {
         "column": "exercises",
         "name": "Solution files for lab 7",
-        "link": "contents/Module_7/Lab/Lab_Code_Solution.zip"
+        "link": "contents/Module 07_Timer, GPIO and 7-Segment Peripherals/Lab/Lab_Code_Solution.zip"
       }
     ],
     [
       {
         "column": "modules",
         "name": "Module 8: Interrupt Mechanisms",
-        "link": "contents/Module_8/Lecture_8_Interrupts.pptx"
+        "link": "contents/Module 08_Interrupt Mechanisms/Lecture_8_Interrupts.pptx"
       },
     {
         "column": "exercises",
         "name": "Lab 8",
-        "link": "contents/Module_8/Lab/IntroSoC_lab08.docx"
+        "link": "contents/Module 08_Interrupt Mechanisms/Lab/IntroSoC_lab08.docx"
       },
     {
         "column": "exercises",
         "name": "Lab 8 Files",
-        "link": "contents/Module_8/Lab/lab_files.zip"
+        "link": "contents/Module 08_Interrupt Mechanisms/Lab/lab_files.zip"
       },
     {
         "column": "exercises",
         "name": "Solution files for lab 8",
-        "link": "contents/Module_8/Lab/Lab_Code_Solution.zip"
+        "link": "contents/Module 08_Interrupt Mechanisms/Lab/Lab_Code_Solution.zip"
       }
     ],
     [
       {
         "column": "modules",
         "name": "Module 9: Programming an SoC using C Language",
-        "link": "contents/Module_9/Lecture_9_using_C.pptx"
+        "link": "contents/Module 09_Programming an SoC using C Language/Lecture_9_using_C.pptx"
       },
     {
         "column": "exercises",
         "name": "Lab 9",
-        "link": "contents/Module_9/Lab/IntroSoC_lab09.docx"
+        "link": "contents/Module 09_Programming an SoC using C Language/Lab/IntroSoC_lab09.docx"
       },
     {
         "column": "exercises",
         "name": "Lab 9 Files",
-        "link": "contents/Module_9/Lab/lab_files.zip"
+        "link": "contents/Module 09_Programming an SoC using C Language/Lab/lab_files.zip"
       },
     {
         "column": "exercises",
         "name": "Solution files for lab 9",
-        "link": "contents/Module_9/Lab/Lab_Code_Solution.zip"
+        "link": "contents/Module 09_Programming an SoC using C Language/Lab/Lab_Code_Solution.zip"
       }
     ],
     [
 	  {
         "column": "modules",
         "name": "Module 10: Arm CMSIS and Software Drivers",
-        "link": "contents/Module_10/Lecture_10_CMSIS.pptx"
+        "link": "contents/Module 10_Arm CMSIS and Software Drivers/Lecture_10_CMSIS.pptx"
       },
     {
         "column": "exercises",
         "name": "Lab 10",
-        "link": "contents/Module_10/Lab/IntroSoC_lab10.docx"
+        "link": "contents/Module 10_Arm CMSIS and Software Drivers/Lab/IntroSoC_lab10.docx"
       },
     {
         "column": "exercises",
         "name": "Lab 10 Files",
-        "link": "contents/Module_10/Lab/lab_files.zip"
+        "link": "contents/Module 10_Arm CMSIS and Software Drivers/Lab/lab_files.zip"
       },
     {
         "column": "exercises",
         "name": "Solution files for lab 10",
-        "link": "contents/Module_10/Lab/Lab_Code_Solution.zip"
+        "link": "contents/Module 10_Arm CMSIS and Software Drivers/Lab/Lab_Code_Solution.zip"
       }
     ],
     [
       {
         "column": "modules",
         "name": "Module 11: Application Programming Interface and Final Application",
-        "link": "contents/Module_11/Lecture_11_API_Snake.pptx"
+        "link": "contents/Module 11_Application Programming Interface and Final Application/Lecture_11_API_Snake.pptx"
       },
     {
         "column": "exercises",
         "name": "Lab 11",
-        "link": "contents/Module_11/Lab/IntroSoC_lab11.docx"
+        "link": "contents/Module 11_Application Programming Interface and Final Application/Lab/IntroSoC_lab11.docx"
       },
     {
         "column": "exercises",
         "name": "Lab 11 Files",
-        "link": "contents/Module_11/Lab/lab_files.zip"
+        "link": "contents/Module 11_Application Programming Interface and Final Application/Lab/lab_files.zip"
       },
     {
         "column": "exercises",
         "name": "Solution files for lab 11",
-        "link": "contents/Module_11/Lab/Lab_Code_Solution.zip"
+        "link": "contents/Module 11_Application Programming Interface and Final Application/Lab/Lab_Code_Solution.zip"
       }
     ],
     [
      {
         "column": "exercises",
         "name": "Lab 12",
-        "link": "contents/Module_12/Lab/IntroSoC_lab12.docx"
+        "link": "contents/Module 12_Final Project/Lab/IntroSoC_lab12.docx"
       },
     {
         "column": "exercises",
         "name": "Lab 12 Files",
-        "link": "contents/Module_12/Lab/lab_files.zip"
+        "link": "contents/Module 12_Final Project/Lab/lab_files.zip"
       },
     {
         "column": "exercises",
         "name": "Solution files for lab 12",
-        "link": "contents/Module_12/Lab/Lab_Code_Solution.zip"
+        "link": "contents/Module 12_Final Project/Lab/Lab_Code_Solution.zip"
       }
     ]
   ]
